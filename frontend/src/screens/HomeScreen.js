cb7b109341563a7be778bc79c250229bf540fe69
Michael Walker <pythonk@email.phoenix.edu>
2022-11-20 15:24:05 -0500

    Product Screen - Section 5
    Implemented more details in the Product Screen component and set it to 3 columns. Added a new API on the server to return the products details screen based on it's slug. Installed react-helmet-async to update the browser tab (title) info. Created a function in the utilities file to handle the display of error messages when the user types in an invalid web page. Created components for MessageBox and LoadingBox that is used across different components.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from "../components/Product";
+import { Helmet } from 'react-helmet-async';
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -37,13 +40,14 @@ export default function HomeScreen() {
     }, []);
     return (
         <div>
+            <Helmet><title>Bamazon</title></Helmet>
             <h1>Featured Products</h1>
             <div className="products">
                 {
                     loading ? (
-                        <div>Loading...</div>
+                        <LoadingBox />
                     ) : error ? (
-                        <div>{error}</div>
+                        <MessageBox variant="danger">{error}</MessageBox>
                     ) : (
                         <Row>
                             {products.map(product => (
