eeca21b3c04d04ac17d1e1b75b74fd85e9ed2e02
Michael Walker <pythonk@email.phoenix.edu>
2022-11-20 12:50:56 -0500

    React Bootstrap - Section 4
    Imported React Bootstrap and used the Card, Row and Col display features to list the products on the home page. Created a component for Product to display image and title on each card. Also created a component for Rating to display the rating and number of reviews on each card. Use a link from Font Awesome to show the ratings and stars and half-stars.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { useEffect, useReducer, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useReducer } from "react";
 import axios from 'axios';
-import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -17,7 +18,7 @@ const reducer = (state, action) => {
 };
 
 export default function HomeScreen() {
-    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
         products: [],
         loading: true,
         error: '',
@@ -44,20 +45,13 @@ export default function HomeScreen() {
                     ) : error ? (
                         <div>{error}</div>
                     ) : (
-                        products.map(product => (
-                            <div className="product" key={product.slug}>
-                                <Link to={`/product/${product.slug}`}>
-                                    <img src={product.image} alt={product.name} />
-                                </Link>
-                                <div className="product-info">
-                                    <Link to={`/product/${product.slug}`}>
-                                        <p>{product.name}</p>
-                                    </Link>
-                                    <p><strong>${product.price}</strong></p>
-                                    <button>Add to cart</button>
-                                </div>
-                            </div>
-                        ))
+                        <Row>
+                            {products.map(product => (
+                                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                    <Product product={product}></Product>
+                                </Col>
+                            ))}
+                        </Row>
                     )}
             </div>
         </div>
